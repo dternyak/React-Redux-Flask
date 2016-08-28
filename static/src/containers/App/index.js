00a58b1ec19181edfee0cb45f3d6dcb05f6298a5
Valerii Iatsko <dwr@codingbox.io>
2016-08-28 20:04:19 +0200

    Made App stateless function

--- static/src/containers/App/index.js
+++ static/src/containers/App/index.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
-
-/* global styles for app */
-import './styles/app.scss';
-
-/* application components */
-import { Header } from 'components/Header';
-import { Footer } from 'components/Footer';
+import React from 'react';
 
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-export class App extends Component {
-    static propTypes = {
-        children: React.PropTypes.any,
-    }
+/* application components */
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
 
-    render() {
-        return (
-            <MuiThemeProvider muiTheme={getMuiTheme()}>
-                <section>
-                    <Header/>
-                    <div className="container" style={{"marginTop": 10, "paddingBottom": 250}}>
-                        {this.props.children}
-                    </div>
-                    <div>
-                        <Footer />
-                    </div>
-                </section>
-            </MuiThemeProvider>
-        );
-    }
-}
+/* global styles for app */
+import './styles/app.scss';
+
+const App = ({ children }) =>
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <section>
+            <Header />
+            <div
+              className="container"
+              style={{ marginTop: 10, paddingBottom: 250 }}
+            >
+                {children}
+            </div>
+            <div>
+                <Footer />
+            </div>
+        </section>
+    </MuiThemeProvider>;
+
+App.propTypes = {
+    children: React.PropTypes.node,
+};
