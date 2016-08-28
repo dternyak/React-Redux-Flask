dc4678037f1d50b5149174b6f3e29724f4b4270f
Valerii Iatsko <dwr@codingbox.io>
2016-08-28 20:49:31 +0200

    Fixed HomeContainer export

--- static/src/containers/HomeContainer/index.js
+++ static/src/containers/HomeContainer/index.js
@@ -3,9 +3,7 @@ import React from 'react';
 /* components */
 import { Home } from '../../components/Home';
 
-const HomeContainer = () =>
+export const HomeContainer = () =>
     <section>
         <Home />
     </section>;
-
-export default HomeContainer;
