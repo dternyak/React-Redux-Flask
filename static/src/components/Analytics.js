7345539eb1850a08d6ca23f3abd11d2000dddcb9
Valerii Iatsko <dwr@codingbox.io>
2016-08-28 19:47:14 +0200

    Made Analytics stateless function

--- static/src/components/Analytics.js
+++ static/src/components/Analytics.js
@@ -1,40 +1,24 @@
-import React from 'react'
+import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actionCreators from '../actions/auth';
-import TextField from 'material-ui/TextField';
-import RaisedButton from 'material-ui/RaisedButton';
-import Paper from 'material-ui/Paper';
-
-import {validateEmail} from '../utils/misc'
 
 function mapStateToProps(state) {
     return {
         isRegistering: state.auth.isRegistering,
-        registerStatusText: state.auth.registerStatusText
-    }
-};
+        registerStatusText: state.auth.registerStatusText,
+    };
+}
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(actionCreators, dispatch)
-};
-
-
-@connect(mapStateToProps, mapDispatchToProps)
-export default class Analytics extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-    }
-
-    render() {
-        return (
-            <div className='col-md-8'>
-                <h1>Analytics</h1>
-                <hr/>
-            </div>
-        );
-
-    }
+    return bindActionCreators(actionCreators, dispatch);
 }
+
+const Analytics = connect(mapStateToProps, mapDispatchToProps)(
+    <div className="col-md-8">
+        <h1>Analytics</h1>
+        <hr />
+    </div>
+);
+
+export default Analytics;
