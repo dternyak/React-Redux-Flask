7d341ad56b3f49e2e9368e2e452416305047f106
Valerii Iatsko <dwr@codingbox.io>
2016-08-28 20:16:24 +0200

    eslint fix routes.js

--- static/src/routes.js
+++ static/src/routes.js
@@ -1,27 +1,28 @@
+/* eslint new-cap: 0 */
+
 import React from 'react';
 import { Route } from 'react-router';
 
 /* containers */
-import { App } from 'containers/App';
-import { HomeContainer } from 'containers/HomeContainer';
-import LoginView from 'components/LoginView'
-import RegisterView from 'components/RegisterView'
-import ProtectedView from 'components/ProtectedView'
-import Analytics from 'components/Analytics'
-import NotFound from 'components/NotFound'
+import { App } from './containers/App';
+import { HomeContainer } from './containers/HomeContainer';
+import LoginView from './components/LoginView';
+import RegisterView from './components/RegisterView';
+import ProtectedView from './components/ProtectedView';
+import Analytics from './components/Analytics';
+import NotFound from './components/NotFound';
 
-import {DetermineAuth} from 'components/DetermineAuth';
-import {requireAuthentication} from 'components/AuthenticatedComponent';
-import {requireNoAuthentication} from 'components/notAuthenticatedComponent';
+import { DetermineAuth } from './components/DetermineAuth';
+import { requireAuthentication } from './components/AuthenticatedComponent';
+import { requireNoAuthentication } from './components/notAuthenticatedComponent';
 
 export default (
     <Route path="/" component={App}>
-        <Route path="main" component={requireAuthentication(ProtectedView)}/>
-        <Route path="login" component={requireNoAuthentication(LoginView)}/>
-        <Route path="register" component={requireNoAuthentication(RegisterView)}/>
-        <Route path="home" component={requireNoAuthentication(HomeContainer)}/>
-        <Route path="analytics" component={requireAuthentication(Analytics)}/>
-        <Route path="*" component={DetermineAuth(NotFound)}/>
-
+        <Route path="main" component={requireAuthentication(ProtectedView)} />
+        <Route path="login" component={requireNoAuthentication(LoginView)} />
+        <Route path="register" component={requireNoAuthentication(RegisterView)} />
+        <Route path="home" component={requireNoAuthentication(HomeContainer)} />
+        <Route path="analytics" component={requireAuthentication(Analytics)} />
+        <Route path="*" component={DetermineAuth(NotFound)} />
     </Route>
 );
