d16877b64722f7b8e37d4fabb11ad689a25fde89
Valerii Iatsko <dwr@codingbox.io>
2016-08-28 20:16:54 +0200

    eslint fix server.js

--- static/server.js
+++ static/server.js
@@ -1,43 +1,43 @@
-var http = require('http');
-var express = require('express'),
-    httpProxy = require('http-proxy');
+const http = require('http');
+const express = require('express');
+const httpProxy = require('http-proxy');
+const path = require('path');
 
-var proxy = httpProxy.createProxyServer({});
+const proxy = httpProxy.createProxyServer({});
 
-var app = express();
+const app = express();
 
 app.use(require('morgan')('short'));
 
 (function initWebpack() {
-  const webpack = require('webpack');
-  const webpackConfig = require('./webpack/common.config');
-  const compiler = webpack(webpackConfig);
+    const webpack = require('webpack');
+    const webpackConfig = require('./webpack/common.config');
 
-  app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true, publicPath: webpackConfig.output.publicPath,
-  }));
+    const compiler = webpack(webpackConfig);
 
-  app.use(require('webpack-hot-middleware')(compiler, {
-    log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000,
-  }));
+    app.use(require('webpack-dev-middleware')(compiler, {
+        noInfo: true, publicPath: webpackConfig.output.publicPath,
+    }));
 
-  app.use(express.static(__dirname + '/'));
-})();
+    app.use(require('webpack-hot-middleware')(compiler, {
+        log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000,
+    }));
 
-app.all(/^\/api\/(.*)/, function api(req, res) {
+    app.use(express.static(path.join(__dirname, '/')));
+}());
+
+app.all(/^\/api\/(.*)/, (req, res) => {
     proxy.web(req, res, { target: 'http://localhost:5000' });
 });
 
-app.get(/.*/, function root(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 
-
-
 const server = http.createServer(app);
-server.listen(process.env.PORT || 3000, function onListen() {
-  const address = server.address();
-  console.log('Listening on: %j', address);
-  console.log(' -> that probably means: http://localhost:%d', address.port);
+server.listen(process.env.PORT || 3000, () => {
+    const address = server.address();
+    console.log('Listening on: %j', address);
+    console.log(' -> that probably means: http://localhost:%d', address.port);
 });
