b86ecaad73151b6bd54452a48143ddbd989d8685
Valerii Iatsko <dwr@codingbox.io>
2016-08-28 20:16:00 +0200

    eslint fix utils/http_functions.js

--- static/src/utils/http_functions.js
+++ static/src/utils/http_functions.js
@@ -1,17 +1,17 @@
+/* eslint camelcase: 0 */
+
 import axios from 'axios';
 
-const tokenConfig = function (token) {
-    return {
-        headers: {
-            'Authorization': token
-        }
-    }
-};
+const tokenConfig = (token) => ({
+    headers: {
+        'Authorization': token, // eslint-disable-line quote-props
+    },
+});
 
 export function validate_token(token) {
     return axios.post('/api/is_token_valid', {
-        token: token,
-    })
+        token,
+    });
 }
 
 export function get_github_access() {
@@ -23,22 +23,22 @@ export function get_github_access() {
 
 export function create_user(email, password) {
     return axios.post('api/create_user', {
-        email: email,
-        password: password
-    })
+        email,
+        password,
+    });
 }
 
 export function get_token(email, password) {
     return axios.post('api/get_token', {
-        email: email,
-        password: password
-    })
+        email,
+        password,
+    });
 }
 
 export function has_github_token(token) {
-    return axios.get('api/has_github_token', tokenConfig(token))
+    return axios.get('api/has_github_token', tokenConfig(token));
 }
 
 export function data_about_user(token) {
-    return axios.get('api/user', tokenConfig(token))
+    return axios.get('api/user', tokenConfig(token));
 }
