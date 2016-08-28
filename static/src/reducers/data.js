0d058e7f7a197882da94b0e956407785133c455e
Valerii Iatsko <dwr@codingbox.io>
2016-08-28 20:16:41 +0200

    eslint fix reducers/data.js

--- static/src/reducers/data.js
+++ static/src/reducers/data.js
@@ -1,26 +1,25 @@
-import {createReducer} from '../utils/misc';
-import {RECEIVE_PROTECTED_DATA, FETCH_PROTECTED_DATA_REQUEST} from '../actions/data';
+import { RECEIVE_PROTECTED_DATA, FETCH_PROTECTED_DATA_REQUEST } from '../actions/data';
 
 const initialState = {
     data: null,
     isFetching: false,
-    loaded: false
+    loaded: false,
 };
 
 export function data(state = initialState, action) {
     switch (action.type) {
-        case 'RECEIVE_PROTECTED_DATA':
-            return Object.assign({}, state, {
-                'data': action.payload.data,
-                'isFetching': false,
-                'loaded': true
-            });
-        case 'FETCH_PROTECTED_DATA_REQUEST':
-            return Object.assign({}, state, {
-                'isFetching': true
-            });
+    case [RECEIVE_PROTECTED_DATA]:
+        return Object.assign({}, state, {
+            data: action.payload.data,
+            isFetching: false,
+            loaded: true,
+        });
+    case [FETCH_PROTECTED_DATA_REQUEST]:
+        return Object.assign({}, state, {
+            isFetching: true,
+        });
 
-        default:
-            return state
+    default:
+        return state;
     }
 }
