93b586c4115a4eabcbb921c788e49ce1c2fefa62
Valerii Iatsko <dwr@codingbox.io>
2016-08-28 20:15:13 +0200

    eslint fix prod.config.js

--- static/webpack/prod.config.js
+++ static/webpack/prod.config.js
@@ -2,35 +2,35 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  devtool: 'source-map',
+    devtool: 'source-map',
 
-  entry: ['bootstrap-loader/extractStyles'],
+    entry: ['bootstrap-loader/extractStyles'],
 
-  output: {
-    publicPath: 'dist/',
-  },
+    output: {
+        publicPath: 'dist/',
+    },
 
-  module: {
-    loaders: [{
-      test: /\.scss$/,
-      loader: 'style!css!postcss-loader!sass',
-    }],
-  },
+    module: {
+        loaders: [{
+            test: /\.scss$/,
+            loader: 'style!css!postcss-loader!sass',
+        }],
+    },
 
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"',
-      },
-      __DEVELOPMENT__: false,
-    }),
-    new ExtractTextPlugin('bundle.css'),
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-    }),
-  ],
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"',
+            },
+            __DEVELOPMENT__: false,
+        }),
+        new ExtractTextPlugin('bundle.css'),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+        }),
+    ],
 };
