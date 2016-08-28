82af4c5b28a26e1425de4c6054856a920fe5ed0d
Valerii Iatsko <dwr@codingbox.io>
2016-08-28 19:53:41 +0200

    eslint fix AuthenticatedComponent

--- static/src/components/AuthenticatedComponent.js
+++ static/src/components/AuthenticatedComponent.js
@@ -1,32 +1,29 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {validate_token} from '../utils/http_functions'
-import * as actionCreators from '../actions/auth';
+import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { browserHistory } from 'react-router'
+import { browserHistory } from 'react-router';
+import * as actionCreators from '../actions/auth';
 
 function mapStateToProps(state) {
     return {
         token: state.auth.token,
         userName: state.auth.userName,
-        isAuthenticated: state.auth.isAuthenticated
-    }
-};
+        isAuthenticated: state.auth.isAuthenticated,
+    };
+}
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(actionCreators, dispatch)
-};
+    return bindActionCreators(actionCreators, dispatch);
+}
 
 
 export function requireAuthentication(Component) {
-
     class AuthenticatedComponent extends React.Component {
-
         componentWillMount() {
             this.checkAuth();
             this.state = {
-                loaded_if_needed: false
-            }
+                loaded_if_needed: false,
+            };
         }
 
         componentWillReceiveProps(nextProps) {
@@ -35,40 +32,37 @@ export function requireAuthentication(Component) {
 
         checkAuth(props = this.props) {
             if (!props.isAuthenticated) {
-                let token = localStorage.getItem('token')
+                const token = localStorage.getItem('token');
                 if (!token) {
-                    browserHistory.push('/home')
-
+                    browserHistory.push('/home');
                 } else {
-                    return fetch('api/is_token_valid', {
+                    fetch('api/is_token_valid', {
                         method: 'post',
                         credentials: 'include',
                         headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json'
+                            'Accept': 'application/json', // eslint-disable-line quote-props
+                            'Content-Type': 'application/json',
                         },
-                        body: JSON.stringify({token: token})
+                        body: JSON.stringify({ token }),
                     })
                         .then(res => {
                             if (res.status === 200) {
-                                this.props.loginUserSuccess(token)
+                                this.props.loginUserSuccess(token);
                                 this.setState({
-                                    loaded_if_needed: true
-                                })
+                                    loaded_if_needed: true,
+                                });
 
                             } else {
-                                browserHistory.push('/home')
+                                browserHistory.push('/home');
 
                             }
-                        })
+                        });
 
                 }
-
-
             } else {
                 this.setState({
-                    loaded_if_needed: true
-                })
+                    loaded_if_needed: true,
+                });
             }
         }
 
@@ -76,15 +70,19 @@ export function requireAuthentication(Component) {
             return (
                 <div>
                     {this.props.isAuthenticated && this.state.loaded_if_needed
-                        ? <Component {...this.props}/>
+                        ? <Component {...this.props} />
                         : null
                     }
                 </div>
-            )
+            );
 
         }
     }
 
-    return connect(mapStateToProps, mapDispatchToProps)(AuthenticatedComponent);
+    AuthenticatedComponent.propTypes = {
+        loginUserSuccess: React.PropTypes.func,
+        isAuthenticated: React.PropTypes.bool,
+    };
 
+    return connect(mapStateToProps, mapDispatchToProps)(AuthenticatedComponent);
 }
