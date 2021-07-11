2906bf92c308e6bf7651527af2bbf35c8ae21022
wrk <wrk15835@gmail.com>
2021-07-11 21:24:05 +0800

    fix a small problem with bcrypt

--- application/models.py
+++ application/models.py
@@ -13,8 +13,8 @@ class User(db.Model):
 
     @staticmethod
     def hashed_password(password):
-        return bcrypt.generate_password_hash(password).decode("utf-8")
-
+        return bcrypt.generate_password_hash(password)
+        
     @staticmethod
     def get_user_with_email_and_password(email, password):
         user = User.query.filter_by(email=email).first()
